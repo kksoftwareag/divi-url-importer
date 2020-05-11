52ba3509c0170e4633588182f0ff756fb4498ee3
Tim <gebauer@kk-software.de>
2020-05-11 12:04:12 +0200

    Initial commit from Create React App

---
+++ includes/__PluginName.php
@@ -0,0 +1,46 @@
+<?php
+
+class URLIMP_UrlImporter extends DiviExtension {
+
+	/**
+	 * The gettext domain for the extension's translations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $gettext_domain = 'urlimp-url-importer';
+
+	/**
+	 * The extension's WP Plugin name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $name = 'url-importer';
+
+	/**
+	 * The extension's version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * URLIMP_UrlImporter constructor.
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public function __construct( $name = 'url-importer', $args = array() ) {
+		$this->plugin_dir     = plugin_dir_path( __FILE__ );
+		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+
+		parent::__construct( $name, $args );
+	}
+}
+
+new URLIMP_UrlImporter;
